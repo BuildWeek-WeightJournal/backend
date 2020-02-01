e0725378e0684d169b9949e79f9aadc3bf212f7e
perezsergioace <perez.sergio.ace@gmail.com>
2020-02-01 16:08:38 -0500

    added Usersmodel to the authRouter

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 
+const Users = require('../Users/usersModel');
+
 const { jwtSecret } = require('../config/secrets');
 
 function signToken(user) {
