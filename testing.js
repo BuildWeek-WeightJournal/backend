2f58a4ff459bde1d4681971e711e5522d08adca7
perezsergioace <perez.sergio.ace@gmail.com>
2020-02-01 15:47:29 -0500

    finished setting up UsersModel with apropriate code

---
+++ testing.js
@@ -0,0 +1,23 @@
+// const Users = require('./Users/usersModel');
+
+// Users.find()
+//     .then(res => {
+//         console.log(res)
+//     })
+
+// Users.findById(1)
+//     .then(response => {
+//         console.log(response)
+//     })
+
+// Users.findBy({ username: 'bob' })
+//     .then(response => {
+//         console.log(response)
+//     })
+// const user = {username: 'user5', password: 'password'}
+
+// Users.add(user)
+//     .then(response => {
+//         console.log(response)
+//         process.exit();
+//     })
