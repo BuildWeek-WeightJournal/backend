d1d426465af4a4fdae2b29a0118924d3011547ac
Michael <skaman4459@gmail.com>
2020-02-01 16:15:33 -0500

    login added

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -6,6 +6,25 @@ const Users = require('../Users/usersModel');
 
 const { jwtSecret } = require('../config/secrets');
 
+
+
+
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(password, user.password)) {
+                const token = signToken(user);
+                res.status(200).json({ message: `Welcome ${user.username}`, token })
+            } else {
+                res.status(401).json({ message: 'User does not exist.' })
+            }
+        })
+})
+
 function signToken(user) {
     const payload = {
         id: user.id,
