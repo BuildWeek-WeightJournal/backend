de8732a453e8f0c6876e7d3eccdc8e614109bc41
Michael <skaman4459@gmail.com>
2020-02-01 17:38:02 -0500

    Added docs

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -21,6 +21,23 @@ router.post('/register', (req, res) => {
         })
 })
 
+/** 
+ * @api {post} /api/auth/register Register User
+ * @apiName RegisterUser
+ * @apiGroup Authentication
+ * 
+ * @apiParam {String} username username (required) 
+ * @apiParam {String} password password (required)
+ * 
+ * @apiSuccessExample successful response: 
+ * http/1.1 201 Created
+ * {
+ *   "id": 4,
+ *   "username": "test",
+ *   "password": "$2a$08$6fu3MlbA4mXGegw3h.m5eegLbRmG7KxkuplTA5lMLWa7shdXZMKYu"
+ * }
+ **/
+
 router.post('/login', (req, res) => {
     const { username, password } = req.body;
 
@@ -39,6 +56,22 @@ router.post('/login', (req, res) => {
             res.status(500).json({ message: 'Error logging in.' })
         })
     })
+
+/** 
+ * @api {post} /api/auth/login Login User
+ * @apiName LoginUser
+ * @apiGroup Authentication
+ * 
+ * @apiParam {String} username username (required) 
+ * @apiParam {String} password password (required)
+ * 
+ * @apiSuccessExample successful response: 
+ * http/1.1 201 Created
+ * {
+ *   "message": "Welcome test",
+ *   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNTgwNTk2NTgxLCJleHAiOjE1ODEyMDEzODF9.dCjB70A25ZCa7wmXhUAtoGKCtFESP8g-BRgdhw6jgG4"
+ * }
+ **/
 
 function signToken(user) {
     const payload = {
