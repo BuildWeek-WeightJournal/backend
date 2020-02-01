b342b9649f443cb1e2a94d859c76ecf453415b7e
Michael <skaman4459@gmail.com>
2020-02-01 15:16:22 -0500

    database setup

--- database/seeds/02-workouts.js
+++ database/seeds/02-workouts.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
 
-      return knex('table_name').insert([
+      return knex('workouts').insert([
         { user_id: 1, name:'test', reps: 1, weight: 1, body_region: 'Legs'},
         { user_id: 2, name:'test', reps: 2, weight: 2, body_region: 'arms'},
         { user_id: 3, name:'test', reps: 3, weight: 3, body_region: 'back'}
