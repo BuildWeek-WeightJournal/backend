20807f6d0594ef6cde6b00dd54d2e205d32ac0a4
Michael <skaman4459@gmail.com>
2020-02-01 14:35:17 -0500

    workouts table created

--- database/migrations/20200201140408_workouts.js
+++ database/migrations/20200201140408_workouts.js
@@ -1,8 +1,26 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('workout', table => {
+      table.increments();
+      table.integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      table.string('name', 128)
+        .notNullable();
+      table.integer('reps');
+      table.integer('weight');
+      table.string('body_region');
+      table.timestamp('date')
+        .notNullable()
+        .defaultTo(knex.fn.now());
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('workout');
 };
