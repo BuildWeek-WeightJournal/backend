94960609fb53894922c81e5b1714025dd6a4e26a
Michael <skaman4459@gmail.com>
2020-02-01 14:58:37 -0500

    added seed files and setup cleaner

---
+++ database/seeds/00-cleanup.js
@@ -0,0 +1,10 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  const cleaner = require('knex-cleaner');
+  exports.seed = function(knex) {
+  return cleaner.clean(knex, {
+    ignoreTables: ['knex_migrations', 'knex_migrations_lock'], // don't empty migration tables
+  });
+};
+};
