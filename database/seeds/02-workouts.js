eff3fc3f19c73dc81863c7e8911f8d75cd2e7268
Michael <skaman4459@gmail.com>
2020-02-02 13:50:32 -0500

    CRUD for workout completed?

--- database/seeds/02-workouts.js
+++ database/seeds/02-workouts.js
@@ -2,8 +2,14 @@
 exports.seed = function(knex) {
 
       return knex('workouts').insert([
-        { user_id: 1, name:'test', reps: 1, weight: 1, body_region: 'Legs'},
-        { user_id: 2, name:'test', reps: 2, weight: 2, body_region: 'arms'},
-        { user_id: 3, name:'test', reps: 3, weight: 3, body_region: 'back'}
+        { user_id: 1, name:'Bench', reps: 5, weight: 500, body_region: 'Chest'},
+        { user_id: 1, name:'Squat', reps: 5, weight: 800, body_region: 'Full body'},
+        { user_id: 1, name:'Deadlift', reps: 8, weight: 1200, body_region: 'Back'},
+        { user_id: 2, name:'Deadlift', reps: 10, weight: 100, body_region: 'Back'},
+        { user_id: 2, name:'Squat', reps: 10, weight: 150, body_region: 'Full body'},
+        { user_id: 2, name:'Bench', reps: 5, weight: 200, body_region: 'Chest'},
+        { user_id: 3, name:'Bench', reps: 15, weight: 100, body_region: 'Chest'},
+        { user_id: 3, name:'Deadlift', reps: 10, weight: 150, body_region: 'Back'},
+        { user_id: 3, name:'Squat', reps: 15, weight: 100, body_region: 'Full body'}
       ]);
 };
