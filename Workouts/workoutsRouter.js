89569b56cc3dc04ae331b58ab7319c2f2ebe89f7
perezsergioace <perez.sergio.ace@gmail.com>
2020-02-02 15:41:45 -0500

    finished documentation?

--- Workouts/workoutsRouter.js
+++ Workouts/workoutsRouter.js
@@ -25,6 +25,39 @@ router.get('/:userId', authentication, (req, res) => {
 		});
 });
 
+/** 
+ * @api {get} /api/workouts/:usedId GET users workouts
+ * @apiName GET Workout
+ * @apiGroup Workouts
+ * 
+ * @apiParam {String} username username, required.
+ * @apiParam {String} password password, required.
+ * 
+ * @apiSuccessExample successful response: 
+ * http/1.1 200 OK
+ * 
+ [
+    {
+        "id": 1,
+        "user_id": 1,
+        "name": "Bench",
+        "reps": 5,
+        "weight": 500,
+        "body_region": "Chest",
+        "date": "2020-02-02 18:29:18"
+    },
+    {
+        "id": 2,
+        "user_id": 1,
+        "name": "Squat",
+        "reps": 5,
+        "weight": 800,
+        "body_region": "Full body",
+        "date": "2020-02-02 18:29:18"
+    },
+]
+ **/
+
 router.post('/:userId', (req, res) => {
     const workout = {...req.body, user_id: req.params.userId};
     Users.findById(req.params.userId)
@@ -48,6 +81,32 @@ router.post('/:userId', (req, res) => {
         })
 })
 
+/** 
+ * @api {post} /api/workouts/:usedId POST new workout
+ * @apiName POST Workout
+ * @apiGroup Workouts
+ * 
+ * @apiParam {String} name name of workout, required.
+ * @apiParam {Integer} reps amount of reps.
+ * @apiParam {Integer} weight amount of weight.
+ * @apiParam {String} body_region name of workout.
+ * 
+ * @apiSuccessExample successful response: 
+ * http/1.1 201 Created
+ * 
+ [
+    {
+    "id": 10,
+    "user_id": 1,
+    "name": "Bench",
+    "reps": 5,
+    "weight": 150,
+    "body_region": "Chest",
+    "date": "2020-02-02 20:28:51"
+    }
+]
+ **/
+
 router.put('/:userId', (req, res) => {
     Workouts.update(req.body.id, req.body)
     .then(updateWorkout => {
@@ -63,6 +122,32 @@ router.put('/:userId', (req, res) => {
     })
 })
 
+/** 
+ * @api {put} /api/workouts/:usedId PUT update workout
+ * @apiName PUT Workout
+ * @apiGroup Workouts
+ * 
+ * @apiParam {String} name name of workout, required.
+ * @apiParam {Integer} reps amount of reps.
+ * @apiParam {Integer} weight amount of weight.
+ * @apiParam {String} body_region name of workout.
+ * 
+ * @apiSuccessExample successful response: 
+ * http/1.1 200 Created
+ * 
+ [
+    {
+    "id": 10,
+    "user_id": 1,
+    "name": "Squat",
+    "reps": 10,
+    "weight": 250,
+    "body_region": "Full Body",
+    "date": "2020-02-02 20:28:51"
+}
+]
+ **/
+
 router.delete('/:workoutId', (req, res) => {
     Workouts.remove(req.params.workoutId)
         .then(() => {
@@ -73,5 +158,18 @@ router.delete('/:workoutId', (req, res) => {
             res.status(500).json({ message: 'Error deleting workout' })
         })
 })
+
+/** 
+ * @api {delete} /api/workouts/:workoutId DELETE workout
+ * @apiName DELETE Workout
+ * @apiGroup Workouts
+ * 
+ * @apiSuccessExample successful response: 
+ * http/1.1 200 OK
+ * 
+{
+    "message": "Workout deleted. Good job."
+}
+ **/
 
 module.exports = router;
