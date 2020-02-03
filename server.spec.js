6fc4f9356af216bb2a3f6937b0a853b363bdd58c
perezsergioace <perez.sergio.ace@gmail.com>
2020-02-03 12:54:33 -0500

    ran server tests, everythings seems to run fine

--- server.spec.js
+++ server.spec.js
@@ -1,12 +1,25 @@
-// const request = require('supertest');
-// const server = require('./server');
+const request = require('supertest');
+const server = require('./server');
 
-// describe('server', function() {
-//     it('runs the tests', function() {
-//         expect(true).toBe(true);
-//     })
+describe('server', function() {
+    it('runs the tests', function() {
+        expect(true).toBe(true);
+    })
 
-//     describe('tests environment', function() {
-//         it()
-//     })
-// })
+    describe('tests environment', function() {
+        it('should use the testing environment', function() {
+            expect(process.env.DB_ENV).toBe('testing')
+        })
+    })
+
+    describe('GET /', function() {
+        it('should return a 200 OK', function() {
+            return request(server)
+                .get('/')
+                .then(res => {
+                    expect(res.status).toBe(200);
+                })
+        })
+    })
+})
+
