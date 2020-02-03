b26352c2664daeaf64c50f57da9399861bdf6e79
perezsergioace <perez.sergio.ace@gmail.com>
2020-02-03 12:47:21 -0500

    setup files for testing. ran knex commands to generate test file

--- knexfile.js
+++ knexfile.js
@@ -26,17 +26,17 @@ module.exports = {
     connection: {
       filename: './database/test.db3'
     },
-    useNullAsDefault: true,
     pool: {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done);
       },
     },
+    useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations'
+      directory: './database/migrations',
     },
     seeds: {
-      directory: './database/migrations'
+      directory: './database/seeds',
     },
   },
 
